8a60be05ba8204cc0b9346303366bbd3869874b0
bhakti <bhaktishinde2388@gmail.com>
2024-07-24 23:02:51 +0530

    imported the usestate and useEffect.then news url passed  in the localNews variable

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -4,10 +4,21 @@ import axios from "axios"
 import "./Home.css"
 
 function Home() {
-   return (
+    const [news , setNews] =useState([])
+
+    const localNews = async() => {
+        const response = await axios.get("https://newsapi.org/v2/everything?q=tesla&from=2024-06-24&sortBy=publishedAt&apiKey=8063435e33c3466c9ecbe13a5737392e");
+      
+    }
+    useEffect(()=>{
+          localNews()
+    },[])
+
+
+  return (
     <div>
+       
        <h1>news</h1>
-  
     </div>
   )
 }
