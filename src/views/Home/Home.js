a4fc5371aa522aee0b02a47162d3a0a7f0fd27f5
bhakti <bhaktishinde2388@gmail.com>
2024-07-24 23:10:37 +0530

    used map for news articles array and the 'read more' btn is added for read more info

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -8,7 +8,7 @@ function Home() {
 
     const localNews = async() => {
         const response = await axios.get("https://newsapi.org/v2/everything?q=tesla&from=2024-06-24&sortBy=publishedAt&apiKey=8063435e33c3466c9ecbe13a5737392e");
-      
+       setNews(response.data.articles)
     }
     useEffect(()=>{
           localNews()
@@ -18,7 +18,33 @@ function Home() {
   return (
     <div>
        
-       <h1>news</h1>
+        <div className='news-cards-container'>
+        {
+            news.map((article,index)=>{
+
+            const {author,title,description,url,urlToImage,publishedAt,content} = article
+
+                return(
+                    
+                    <div className="news-card" key={index}>
+                     <img src={urlToImage} className='news-article-img'/>
+                     <h2 className="news-title">{title}</h2>
+
+                     <div className="article-body">
+                     <span className="news-article-author">{author}</span>
+                     <span className='published'>{publishedAt}</span>
+                    </div>
+
+                    <p className="news-description">{description}</p>
+
+                    <a href={url} target='_blank' className="readmore-btn">Read More</a>
+
+                    </div>
+                    
+                )
+            })
+        }
+        </div>
     </div>
   )
 }
